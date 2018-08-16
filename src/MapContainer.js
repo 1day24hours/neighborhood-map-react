5ac391505e313ab8fb7b45993012ce9c923fabc5
Felix Chen <chen.felix.xi@gmail.com>
2018-08-17 03:19:01 +0800

    3rd api

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-const foursquare = require('react-foursquare')({
-    clientID: 'HNWME22DACZBRE3XCHEPDICRSCBQWCN4LSK3FRPO0VHEFFED',
-    clientSecret: '0DUHNOBRKVA0AW0PHJK4AE40L02UIWEGOOIVWJW3FS1VCAPR'
-});
+// const foursquare = require('react-foursquare')({
+//     clientID: 'HNWME22DACZBRE3XCHEPDICRSCBQWCN4LSK3FRPO0VHEFFED',
+//     clientSecret: '0DUHNOBRKVA0AW0PHJK4AE40L02UIWEGOOIVWJW3FS1VCAPR'
+// });
 
 export class MapContainer extends Component {
     constructor(props){
@@ -48,6 +48,56 @@ export class MapContainer extends Component {
         })
     };
 
+    getMakerInfo= (marker) => {
+
+        // Add the api keys for foursquare
+        const clientId = 'HNWME22DACZBRE3XCHEPDICRSCBQWCN4LSK3FRPO0VHEFFED';
+        const clientSecret = '0DUHNOBRKVA0AW0PHJK4AE40L02UIWEGOOIVWJW3FS1VCAPR';
+
+        // Build the api endpoint
+        const url =
+            'https://api.foursquare.com/v2/venues/search?client_id=' +
+            clientId +
+            '&client_secret' +
+            clientSecret +
+            '&v=20130815&ll=' +
+            marker.getPosition().lat() +
+            ',' +
+            marker.getPosition().lng() +
+            'limit=1';
+        fetch(url)
+            .then(response => {
+                if (response.status !== 200) {
+                    this.state.infoWindow.setContent('Sorry data can\'t be loaded');
+                    return;
+                }
+                response.this.state.place.then(data => {
+                    console.log(data);
+
+                    const location_data = data.venues[0];
+                    const place = `<h3>${location_data.name}</h3>`;
+                    const street = `<p>${location_data.formattedAddress[0]}</p>`;
+                    let contact = '';
+                    if (location_data.contact.phone) {
+                        contact = `<p>${location_data.contact.phone}</p>`;
+                    }
+                    const checkInCount =
+                        '<b>Number of CheckIn: </b>' +
+                        location_data.stats.checkinsCount +
+                        '<br>';
+                    const readMore =
+                        '<a href="https://foursquare.com/v/' +
+                        location_data.id +
+                        '" target="_blank">Read More on <b>Foursquare Website</b></a>';
+                    this.state.infoWindow.setContent(
+                        place + street + checkInCount + readMore
+                    );
+                });
+            }).catch(err => {
+                this.state.infoWindow.setContent('Sorry data can\'t be loaded.');
+            });
+    }
+
     render() {
         const { places, google } = this.props;
         const { activeMaker, showingInfoWindow, selectedPlace } = this.state;
@@ -79,6 +129,7 @@ export class MapContainer extends Component {
             <InfoWindow 
             marker={activeMaker} 
             visible={showingInfoWindow}
+            getMakerInfo={this.getMakerInfo}
             >
             <div>
                 {/* <h1>{selectedPlace.name}</h1> */}
