c991e8217a0602cf624d142c26ad2e549ea4878e
Felix Chen <chen.felix.xi@gmail.com>
2018-08-16 20:00:44 +0800

    finished SiteHeader

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,2 +1,16 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    render()
+}
+
+export default GoogleApiWrapper({
+    akiKey: 'AIzaSyCfdB53Ke2w9uJRVoxmj0HNrzVa0WNW84o'
+})(MapContainer);
---
+++ src/SiteHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class SiteHeader extends Component {
+    render() {
+        return(
+            <header className='site-header'>
+                <h1>Neighborhood Map</h1>
+            </header>
+        )
+    } 
+}
+
+export default SiteHeader;
