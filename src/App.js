943f6f3381885a8d4ad6cfb501c29a7e49e38be7
Felix Chen <chen.felix.xi@gmail.com>
2018-08-16 19:35:35 +0800

    finished layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component ,Fragment} from 'react';
+import MapContainer from "./MapContainer";
+import SiteHeader from "./SiteHeader";
+import SiteSearch from "./SiteSearch";
+
+import {places} from './data/places';
 import './App.css';
 
 class App extends Component {
+  state = {
+    places = [],
+    selectedPlace: {}
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { places,selectedPlace } = this.state;
+    return(
+      <Fragment>
+        <SiteHeader />
+        <SiteSearch />
+        <MapContainer />
+      </Fragment>
+    )
   }
 }
 
