f3c83427346ea5180c146679bf8e3a9f473d6b88
Felix Chen <chen.felix.xi@gmail.com>
2018-08-16 19:20:20 +0800

    finished places data

---
+++ src/data/places.js
@@ -0,0 +1,37 @@
+export const places = [
+    {
+        name:'Home',
+        location:{
+            lat: -36.798376,
+            lng: 174.73672
+        }
+    },
+    {
+        name: 'Northcote Baptist Church',
+        location: {
+            lat: -36.795293,
+            lng: 174.735701
+        }
+    },
+    {
+        name: 'Auckland Zoo',
+        location: {
+            lat: -36.864113,
+            lng: 174.719685
+        }
+    },
+    {
+        name: 'OP Shop',
+        location: {
+            lat: -36.799787,
+            lng: 174.747666
+        }
+    },
+    {
+        name: 'Auckland City Backpackers',
+        location: {
+            lat: -36.856863,
+            lng: 174.762632
+        }
+    } 
+];
